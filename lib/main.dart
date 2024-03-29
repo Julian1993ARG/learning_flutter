df397c7937a1725626e464cdbef9790976d73451
julian <julianmartinez_93@hotmail.com>
2024-03-29 11:10:49 -0300

    add theme color

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:hello_word_app/src/presentation/screens/counter_screen_.dart';
 
-void Main() {
-  runApp(MyApp());
+void main() {
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text("hello world"),
+      debugShowCheckedModeBanner: false,
+      home: const CounterScreen(),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.purple,
+          brightness: Brightness.dark,
+        ),
+      ),
     );
   }
 }
