e76f830e63512ec44b5dc88b2591858d8b4f2825
julian <julianmartinez_93@hotmail.com>
2024-03-29 12:14:58 -0300

    add actions appbar and colum to floatingbuttons

--- lib/src/presentation/screens/counter_screen_.dart
+++ lib/src/presentation/screens/counter_screen_.dart
@@ -16,6 +16,16 @@ class _CounterScreenState extends State<CounterScreen> {
         appBar: AppBar(
           title: const Text("Counter Screen"),
           centerTitle: true,
+          actions: [
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  _counter = 0;
+                });
+              },
+              icon: const Icon(Icons.refresh),
+            )
+          ],
         ),
         body: Center(
           child: Column(
@@ -24,16 +34,29 @@ class _CounterScreenState extends State<CounterScreen> {
               Text("$_counter",
                   style: const TextStyle(
                       fontSize: 160, fontWeight: FontWeight.w100)),
-               Text("Click${ _counter == 1 ? "" : "s" }", style: const TextStyle(fontSize: 24)),
+              Text("Click${_counter == 1 ? "" : "s"}",
+                  style: const TextStyle(fontSize: 24)),
             ],
           ),
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            _counter++;
-            setState(() {});
-          },
-          child: const Icon(Icons.plus_one),
+        floatingActionButton: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            FloatingActionButton(
+              onPressed: () {
+                _counter++;
+                setState(() {});
+              },
+              child: const Icon(Icons.plus_one),
+            ),
+             FloatingActionButton(
+              onPressed: () {
+                if(_counter > 0) _counter--;
+                setState(() {});
+              },
+              child: const Icon(Icons.exposure_minus_1),
+            )
+          ],
         ));
   }
 }
